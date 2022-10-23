86b1b7df747241398180afb60aab1bc509a41b10
onady <onady.dev@gmail.com>
2022-10-23 22:04:45 +0900

    샘플 코드 작성

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var user = require('./api/sample')
 
-var users = [
-    {id:1, name:"user1"},
-    {id:2, name:"user2"},
-    {id:3, name:"user3"}
-]
-
-app.use(morgan('dev'));
+if(process.env.NODE_ENV !== 'test'){
+  app.use(morgan('dev'));
+}
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/users', function (req, res) {
-    req.query.limit = req.query.limit || 10;
-    const limit = parseInt(req.query.limit, 10)
-    if(Number.isNaN(limit)){
-        return res.status(400).end();
-    }
-    res.json(users.slice(0, limit));
-  });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.use('/users', user);
 
 module.exports = app;
