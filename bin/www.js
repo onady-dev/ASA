030787c3f3cb5e747d995c45ccfc4799c84ccb52
onady <onady.dev@gmail.com>
2022-11-09 05:36:44 +0900

    typescript 적용

--- bin/www.js
+++
@@ -1,9 +0,0 @@
-const app = require('../')
-const syncDB = require('./sync-db');
-
-syncDB().then(_=> {
-  console.log('Sync database!')
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-  });
-})
