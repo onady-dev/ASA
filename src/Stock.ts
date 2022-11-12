d1ab62c028fb2bc8ecf329b39d7f8c62642c09f5
onady <onady.dev@gmail.com>
2022-11-12 19:38:43 +0900

    stock 클래스 생성

--- src/Stock.ts
+++ src/Stock.ts
@@ -1,24 +1,35 @@
 export class Stock {
-  private name: string;
-  private setValue: number;
-  private highOrLow: string;
+  private _name: string;
+  private _setValue: number;
+  private _highOrLow: string;
 
   constructor(name: string, setValue: number, highOrLow: string) {
-    this.name = name;
-    this.setValue = setValue;
-    this.highOrLow = highOrLow;
+    this._name = name;
+    this._setValue = setValue;
+    this._highOrLow = highOrLow;
     this.validatePositive();
+    this.validateHighLow();
   }
 
   private validatePositive() {
-    if (this.setValue < 0) {
-      throw new Error(`설정 값은 음수가 될 수 없습니다. setValue = ${this.setValue}`);
+    if (this._setValue < 0) {
+      throw new Error(`설정 값은 음수가 될 수 없습니다. setValue = ${this._setValue}`);
     }
   }
 
   private validateHighLow() {
-    if (this.highOrLow === "high" || this.highOrLow === "low") {
-      throw new Error(`high 또는 low만 선택할 수 있습니다. highOrLow = ${this.highOrLow}`);
+    if (this._highOrLow !== "high" && this._highOrLow !== "low") {
+      throw new Error(`high 또는 low만 선택할 수 있습니다. highOrLow = ${this._highOrLow}`);
     }
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  get setValue(): number {
+    return this._setValue;
+  }
+  get highOrLow(): string {
+    return this._highOrLow;
   }
 }
--- test/Stock.spec.ts
+++ test/Stock.spec.ts
@@ -1,12 +1,46 @@
 import { expect } from "chai";
-import exp from "constants";
 import { Stock } from "../src/Stock";
 
 describe("Stock", () => {
-  it("setValue 인수에 음수가 들어가면 에러가 발생한다.", () => {
+  it("2번째(setValue) 인수에 음수가 들어가면 에러가 발생한다.", () => {
     const name = "삼성전자";
     const setValue = -1;
     const highOrLow = "high";
-    expect(new Stock(name, setValue, highOrLow)).to.throw(new Error("설정 값은 음수가 될 수 없습니다. setValue = -1"));
+    expect(() => {
+      new Stock(name, setValue, highOrLow);
+    }).to.throw();
+  });
+
+  it("3번째(highOrLow) 인수에 'high' 또는 'low'가 아닌 문자열이 들어가면 에러가 발생한다.", () => {
+    const name = "삼성전자";
+    const setValue = 100;
+    const highOrLow = "test";
+    expect(() => {
+      new Stock(name, setValue, highOrLow);
+    }).to.throw();
+  });
+
+  it("name 가져오기", () => {
+    const name = "삼성전자";
+    const setValue = 100000;
+    const highOrLow = "high";
+    const stock = new Stock(name, setValue, highOrLow);
+    expect(stock.name).to.equal("삼성전자");
+  });
+
+  it("setValue 가져오기", () => {
+    const name = "삼성전자";
+    const setValue = 100000;
+    const highOrLow = "high";
+    const stock = new Stock(name, setValue, highOrLow);
+    expect(stock.setValue).to.equal(100000);
+  });
+
+  it("highOrLow 가져오기", () => {
+    const name = "삼성전자";
+    const setValue = 100000;
+    const highOrLow = "high";
+    const stock = new Stock(name, setValue, highOrLow);
+    expect(stock.highOrLow).to.equal("high");
   });
 });
