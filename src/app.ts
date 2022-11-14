7ae49735d43ff2a3dddbe3d441faa5476916da82
onady <onady.dev@gmail.com>
2022-11-15 03:23:51 +0900

    -

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const stock = new Stock("카카오", 58600);
 const URL = "https://kr.investing.com/equities/kakao-corp";
 const SELECTOR = "#__next > div > div > div > div > main > div > div > div:nth-child(2) > div > span";
 
-setInterval(crawl, 2000, URL, SELECTOR);
+// setInterval(crawl, 2000, URL, SELECTOR);
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("hello");
